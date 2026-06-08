174c98141c9ba33f9da6782fd934f02ce52a146d
goral006 <mariusz.ziaja@interia.pl>
2026-06-08 12:28:57 +0200

    Update telegram.py

--- telegram.py
+++ telegram.py
@@ -4,14 +4,12 @@ import os
 TOKEN = os.getenv("TOKEN")
 CHAT_ID = os.getenv("CHAT_ID")
 
-def send_telegram(text):
+
+def send_telegram(message):
     url = f"https://api.telegram.org/bot{TOKEN}/sendMessage"
 
-    r = requests.post(url, data={
+    requests.post(url, data={
         "chat_id": CHAT_ID,
-        "text": text,
+        "text": message,
         "parse_mode": "HTML"
     })
-
-    print("Telegram status:", r.status_code)
-    print("Telegram response:", r.text)
