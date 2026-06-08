3a83f319e95da963248f157acd45a6cc014c0339
goral006 <mariusz.ziaja@interia.pl>
2026-06-08 08:54:18 +0200

    Update main.py

--- main.py
+++ main.py
@@ -1,1 +1,16 @@
-print("NOWA_WERSJA_12345")
+import requests
+import os
+
+TOKEN = os.getenv("TOKEN")
+CHAT_ID = os.getenv("CHAT_ID")
+
+r = requests.post(
+    f"https://api.telegram.org/bot{TOKEN}/sendMessage",
+    data={
+        "chat_id": CHAT_ID,
+        "text": "✅ Bot działa po aktualizacji"
+    }
+)
+
+print(r.status_code)
+print(r.text)
