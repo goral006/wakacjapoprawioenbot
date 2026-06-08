c792eac20ae1ca672bdb7bca1e081730b1184588
goral006 <mariusz.ziaja@interia.pl>
2026-06-08 13:02:37 +0200

    Update telegram.py

--- telegram.py
+++ telegram.py
@@ -4,10 +4,13 @@ import os
 TOKEN = os.getenv("TOKEN")
 CHAT_ID = os.getenv("CHAT_ID")
 
+
 def send_telegram(msg):
     url = f"https://api.telegram.org/bot{TOKEN}/sendMessage"
+
     requests.post(url, data={
         "chat_id": CHAT_ID,
         "text": msg,
-        "parse_mode": "HTML"
+        "parse_mode": "HTML",
+        "disable_web_page_preview": True
     })
